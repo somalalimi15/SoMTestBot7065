22c2f361483bd37b0c0d6164550262c36093aa46
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-09 19:22:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -818,6 +818,30 @@ suggestchannel.send("@everyone || @here ");
 }
 });
  
+
+ const sWlc = {}
+client.on('message', message => {
+ 
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "welcome"
+}
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcomer")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+  }
+});
+ 
+ 
+ 
+ 
+ 
+
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
     channel: "welcome"
