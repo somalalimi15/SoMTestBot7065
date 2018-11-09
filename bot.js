ea4484f8554201e5459a98988747e1000307d6db
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-09 19:19:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -818,6 +818,84 @@ suggestchannel.send("@everyone || @here ");
 }
 });
  
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "welcome"
+  }
+  const channel = sWlc[member.guild.id].channel
+    const sChannel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find('name', sChannel);
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+           var mrx = member.user;
+  let mrxembed = new Discord.RichEmbed()
+  .setTitle(`:id: ${mrx.id}  :hash:${mrx.discriminator}`)
+  .addField('» مضى على دخولك الديسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+  .addField('» مضى على دخولك للسيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)  
+  .addField('» انت رقم',`${member.guild.memberCount}`)
+  .setFooter(client.user.username,client.user.avatarURL)
+     welcomer.send({embed:mrxembed});          
+         
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+     
+      const w = ['welcome_4.png'];
+     
+              let Image = Canvas.Image,
+                  canvas = new Canvas(557, 241),
+                  ctx = canvas.getContext('2d');
+ 
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 540, 230);
+     
+      })
+     
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+     
+                                    ctx.font = '21px kathen';
+                              ctx.fontSize = '25px';
+                              ctx.fillStyle = "#FFFFFF";
+                                ctx.fillText(member.user.username, 240, 150);
+                             
+                              //NAMEً
+                              ctx.font = '21px kathen';
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#FFFFFF";
+      ctx.fillText(`Welcome To ${member.guild.name}`, 240, 90);
+     
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
+                   ctx.closePath();
+                   
+                                 ctx.clip();
+ 
+                        ctx.drawImage(ava, 7, 8, 227, 225);
+                              ctx.closePath();
+ 
+                           
+    welcomer.sendFile(canvas.toBuffer())
+     
+     
+     
+      })
+      })
+     
+      }
+      });
 
 
 client.login(process.env.BOT_TOKEN); 
